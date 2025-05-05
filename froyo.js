027091041b1fa622eab3d50be424e75bccb6520b
D3zD3adWinn3r <Desireenlopez5@gmail.com>
2025-05-04 19:51:52 -0500

    commit 1

---
+++ froyo.js
@@ -0,0 +1,16 @@
+// create a list of ice cream flavors
+// crate a function that will remebr the order of ice cream written in the form of a string
+
+const flavor =  [vanilla, vanilla, vanilla, strawberry, coffee, coffee]
+// 'chocolate, vanilla, strawberry, mint, cookie dough, rocky road, butter pecan, pistachio, mango, raspberry, blueberry'
+//create a function that will remember the order of ice cream written in the form of a string
+
+function iceCreamOrder() {
+   
+
+
+    //return the order
+    return order
+}
+
+
