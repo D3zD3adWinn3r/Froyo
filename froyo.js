a8c68b8f47b760abe29db0bb424ecd3ea27e353c
D3zD3adWinn3r <Desireenlopez5@gmail.com>
2025-05-06 20:24:54 -0500

    beep

--- froyo.js
+++ froyo.js
@@ -1,16 +1,25 @@
 // create a list of ice cream flavors
-// crate a function that will remebr the order of ice cream written in the form of a string
+// create a function that will remember the order of ice cream written in the form of a string
 
-const flavor =  [vanilla, vanilla, vanilla, strawberry, coffee, coffee]
+const flavor =  ['vanilla', 'vanilla', 'vanilla', 'strawberry', 'coffee', 'coffee']
 // 'chocolate, vanilla, strawberry, mint, cookie dough, rocky road, butter pecan, pistachio, mango, raspberry, blueberry'
 //create a function that will remember the order of ice cream written in the form of a string
 
-function iceCreamOrder() {
-   
+function iceCreamOrder(array) {
+    // Define the order variable as the flavor array
+    const order = flavor;
+    for ( let i = 0; i < array.length; i++) {
 
+    console.log(array[i]);
+    }
 
-    //return the order
-    return order
+    // Return the order
+    return order;
 }
 
+const userInput = prompt('What is your favorite ice cream flavor?', "vanilla, vanilla, vanilla, strawberry, coffee, coffee");
+const userarray = userInput.split(",")
+// console.log(userarray);
+// console.log(userInput);
 
+iceCreamOrder(userarray)
